d29abed8d09d27a376ad9f5e5a6de42cf94f9885
onggi <hsk1533@naver.com>
2022-03-19 01:08:10 +0900

    Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import './App.css';
 
+function Header(){
+  return (
+      <header>
+          <h1><a href='/'>WEB</a></h1>
+      </header>
+  );
+}
+
+function Nav(){
+  return(
+      <nav>
+        <ol>
+          <li><a href='/read1'>html</a></li>
+          <li><a href='/read2'>css</a></li>
+          <li><a href='/read3'>js</a></li>
+        </ol>
+      </nav>
+  );
+}
+
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello, WEB
+    </article>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <div className='App'>
-        React File Init
-      </div>
+    <div>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
