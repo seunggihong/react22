8cd33933f7825dcd41a872d1848b8b434a776959
onggi <hsk1533@naver.com>
2022-03-20 00:47:58 +0900

    CRUD in Update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Article from "./utiles/Article";
 import Nav from "./utiles/Nav";
 import "./App.css";
 import Create from "./utiles/Create";
+import Update from "./utiles/Update";
 
 function App() {
   const [mode, setMode] = useState("Welcome");
@@ -16,6 +17,7 @@ function App() {
   ]);
 
   let content = null;
+  let contextControl = null;
 
   if (mode === "Welcome") {
     content = <Article title="Welcome" body="Hello. WEB"></Article>;
@@ -29,6 +31,19 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>;
+    contextControl = (
+      <li>
+        <a
+          href={"/update/" + id}
+          onClick={(event) => {
+            event.preventDefault();
+            setMode("UPDATE");
+          }}
+        >
+          Update
+        </a>
+      </li>
+    );
   } else if (mode === "CREATE") {
     content = (
       <Create
@@ -41,6 +56,33 @@ function App() {
           setNextId(nextId + 1);
         }}
       ></Create>
+    );
+  } else if (mode === "UPDATE") {
+    let title,
+      body = null;
+    for (let index = 0; index < topics.length; index++) {
+      if (topics[index].id === id) {
+        title = topics[index].title;
+        body = topics[index].body;
+      }
+    }
+    content = (
+      <Update
+        title={title}
+        body={body}
+        onUpdate={(title, body) => {
+          const updateTopic = { id: id, title: title, body: body };
+          const newTopics = [...topics];
+          for (let index = 0; index < newTopics.length; index++) {
+            if (newTopics[index].id === id) {
+              newTopics[index] = updateTopic;
+              break;
+            }
+          }
+          setTopics(newTopics);
+          setMode("READ");
+        }}
+      ></Update>
     );
   }
 
@@ -72,6 +114,7 @@ function App() {
             Create
           </a>
         </li>
+        {contextControl}
       </ul>
     </div>
   );
